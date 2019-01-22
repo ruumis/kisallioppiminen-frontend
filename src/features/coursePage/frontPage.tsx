2bb8ef38f2cd871ea1031c6707f3b226bc0bbfb1
Marko Koskinen <markokoskinen2037@gmail.com>
2019-01-22 14:13:04 +0200

    frontpage uses base-css file

--- src/features/coursePage/frontPage.tsx
+++ src/features/coursePage/frontPage.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import { InitialState } from '../../types/InitialState'
 import Navigation from '../frontpage/Navigation'
 import Hero from '../frontpage/Hero'
+import CourseList from '../frontpage/CourseList'
 
 export function frontPage(initialState: InitialState) {
   return (
     <div>
       <Navigation />
       <Hero />
+      {/* Saatavilla olevat kurssit: */}
+      <CourseList />
     </div>
   )
 }
