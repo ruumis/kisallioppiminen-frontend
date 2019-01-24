3d6e3dd43bd016ea57326c037517eb148d41354f
Marko Koskinen <markokoskinen2037@gmail.com>
2019-01-24 13:54:13 +0200

    remove tex stuff

--- src/features/coursePage/components/CourseHeader.tsx
+++ src/features/coursePage/components/CourseHeader.tsx
@@ -1,17 +1,18 @@
 import React, { Component } from 'react'
 import { Col, Card } from 'react-materialize'
 import ReactMarkdown from 'react-markdown'
-import {Tex} from 'react-tex'
 
 export class CourseHeader extends Component<{ headerContent: string }> {
   render() {
     return (
       <div className="container">
         <Col m={6} s={12}>
-          <Card className="teal acent-2" textClassName="white-text" title="Osa 1">
-            <ReactMarkdown source={this.props.headerContent} renderers={{
-              code: (props: any) => props.language === 'latex' ?  (<Tex texContent={props.value} />) : <code>{props.value}</code>
-            }} />
+          <Card
+            className="teal acent-2"
+            textClassName="white-text"
+            title="Osa 1"
+          >
+            <ReactMarkdown source={this.props.headerContent} />
           </Card>
         </Col>
       </div>
