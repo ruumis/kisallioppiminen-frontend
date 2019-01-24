299845c5c125c953ceef621f84a7d89e3f6df69f
Marko Koskinen <markokoskinen2037@gmail.com>
2019-01-24 13:40:44 +0200

    add facebook icon

--- src/features/components/Footer.tsx
+++ src/features/components/Footer.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { FaFacebook } from 'react-icons/fa'
+import { IconContext } from 'react-icons'
 
 export default function Footer() {
   return (
@@ -17,19 +19,38 @@ export default function Footer() {
             src="https://seeklogo.com/images/F/facebook-logo-966BBFBC34-seeklogo.com.png"
             alt="fb-icon"
           /> */}
-          <a href="https://www.facebook.com/groups/899303056853769/">
+
+          <IconContext.Provider
+            value={{
+              color: 'blue',
+              className: 'global-class-name'
+            }}
+          >
+            <div>
+              <FaFacebook size={40} />
+            </div>
+          </IconContext.Provider>
+
+          <a
+            className="fb_link"
+            href="https://www.facebook.com/groups/899303056853769/"
+          >
             Facebook-ryhmä
           </a>
         </div>
         <div style={{ width: '33%' }}>
           <p>
-            <img src="/img/copyright.png" alt="Creative Commons copyright" />
+            <img src="/img/CC.png" alt="Creative Commons copyright" />
+            <br />
             Sivuston materiaali on lisensoitu Creative Commons
             BY-NC-SA-lisenssillä ellei muuta mainita.
           </p>
           <p>
             Sivuston lähdekoodi löytyy
-            <a href="https://github.com/kisallioppiminen">GitHubista</a>.
+            <a className="link" href="https://github.com/kisallioppiminen">
+              GitHubista
+            </a>
+            .
           </p>
         </div>
       </footer>
