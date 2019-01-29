76e7947e13e1389d77f446e6ad9d2eb3e2585ddc
Etsku520 <eetu.huovila@gmail.com>
2019-01-29 13:10:53 +0200

    correct section style

--- src/features/coursePage/components/Section.tsx
+++ src/features/coursePage/components/Section.tsx
@@ -1,21 +1,9 @@
 import React from 'react'
 import './styles/Section.scss'
 
-const Section = ({
-  header,
-  renderSection,
-  selected
-}: {
-  header: string
-  renderSection: () => void
-  selected: boolean
-}) =>
+const Section = ({ header, renderSection, selected }: { header: string; renderSection: () => void; selected: boolean }) =>
   selected ? (
-    <div
-      style={{ backgroundColor: '#30A491' }}
-      className="section"
-      onClick={renderSection}
-    >
+    <div style={{ backgroundColor: '#30A491' }} className="section" onClick={renderSection}>
       {header}
     </div>
   ) : (
