3cee36fad60cde54aa72ce2ed6b86949048b9afa
Hugo Holmqvist <hugo.holm@live.com>
2019-01-31 14:10:13 +0200

    Remove unused components

--- src/features/coursePage/coursePage.tsx
+++ src/features/coursePage/coursePage.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import { InitialState } from '../../types/InitialState'
-import { CourseHeader } from './components/CourseHeader'
 
 export function coursePage(initialState: InitialState) {
   return (
     <div>
       <h1>Kurssisivu</h1>
-      <CourseHeader headerContent={initialState.courseHeaderMarkdown} />
+      {initialState.courseHeaderMarkdown}
     </div>
   )
 }
