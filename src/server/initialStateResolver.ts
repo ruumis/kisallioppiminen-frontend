35f0691b17916ae255c26f20904b1dfc11e5bf19
Eero Lahdenperä <eero.lahdenpera@gmail.com>
2019-01-31 13:12:28 +0200

    reading idyll from file

--- src/server/initialStateResolver.ts
+++ src/server/initialStateResolver.ts
@@ -6,7 +6,18 @@ interface ContentConfig {
   chapters: Array<{ number: number; content: string }>
 }
 
+
 const contentConfig: ContentConfig = JSON.parse(fs.readFileSync('./content/content_config.json', 'utf8'))
+// const idyllConfig: ContentConfig = JSON.parse(fs.readFileSync('./content/index.idl', 'utf-8'))
+
+export function getIdyllContent(): InitialState {
+  const content = fs.readFileSync('./content/index.idl', 'utf-8')
+  console.log(content)
+  return {
+    courseHeaderMarkdown: content || '',
+    chapters: []
+  }
+}
 
 export function resolveInitialState(path: string): InitialState {
   const header = getCourseHeader()
