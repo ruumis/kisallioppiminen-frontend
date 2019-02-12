cbfa7edf790e489420c86dec5116302fed933a6a
Hugo Holmqvist <hugo.holm@live.com>
2019-02-12 14:26:28 +0200

    Make course sections links clickable

--- src/features/frontpage/components/Course.tsx
+++ src/features/frontpage/components/Course.tsx
@@ -17,16 +17,20 @@ export default class Course extends Component<Props, any> {
           <h2 className="course-title">{courseName}</h2>
         </Link>
         <span className="course-version">{version}</span>
-        <ol className="course-parts">{createQuickLinks(quickLinks)}</ol>
+        <ol className="course-parts">{createQuickLinks(quickLinks, id)}</ol>
       </div>
     )
   }
 }
 
-function createQuickLinks(links: string[]) {
+function createQuickLinks(links: string[], courseId: string) {
   return links.map((link, index) => (
     <li key={index}>
-      <span className="course-content">{link}</span>
+      <Link href={`/courses/${courseId}/tab/${index}`}>
+        <div className="course-content">
+          {link}
+        </div>
+      </Link>
     </li>
   ))
 }
