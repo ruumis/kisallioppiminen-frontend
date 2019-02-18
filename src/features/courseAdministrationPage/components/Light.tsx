b1b7d96bb50678ced30003814384436efdc17d84
Eelis <32396100+ejronty@users.noreply.github.com>
2019-02-18 15:27:14 +0200

    Scoreboard is now a static table

--- src/features/courseAdministrationPage/components/Light.tsx
+++ src/features/courseAdministrationPage/components/Light.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Light = ({ color }: { color: string }) => {
   const name = 'scoreboard-light-' + color
   return (
-    <span className={name}></span>
+    <div className={name}></div>
   )
 }
 
--- src/features/courseAdministrationPage/components/Scoreboard.tsx
+++ src/features/courseAdministrationPage/components/Scoreboard.tsx
@@ -1,16 +1,44 @@
 import React from 'react'
-import Light from './Light';
+import Light from './Light'
 
 export default function Scoreboard() {
   return (
     <div className="scoreboard">
-      <p>
-        piiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiitkä teksti
-      </p>
-      <Light color="green" />
-      <Light color="yellow" />
-      <Light color="red" />
-      <Light color="gray" />
+      <table>
+        <tr>
+          <th>username</th>
+          <th>1</th>
+          <th>2</th>
+          <th>3</th>
+          <th>4</th>
+          <th>5</th>
+          <th>6</th>
+          <th>7</th>
+          <th>8</th>
+        </tr>
+        <tr>
+          <td>testi Teppo</td>
+          <td><Light color="green" /></td>
+          <td><Light color="gray" /></td>
+          <td><Light color="yellow" /></td>
+          <td><Light color="gray" /></td>
+          <td><Light color="gray" /></td>
+          <td><Light color="red" /></td>
+          <td><Light color="gray" /></td>
+          <td><Light color="gray" /></td>
+        </tr>
+        <tr>
+          <td>koehenkilo</td>
+          <td><Light color="green" /></td>
+          <td><Light color="gray" /></td>
+          <td><Light color="gray" /></td>
+          <td><Light color="green" /></td>
+          <td><Light color="gray" /></td>
+          <td><Light color="gray" /></td>
+          <td><Light color="red" /></td>
+          <td><Light color="yellow" /></td>
+        </tr>
+      </table>
     </div>
   )
 }
