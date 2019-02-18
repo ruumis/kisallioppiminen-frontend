7758f5c3575c9f7b3a7ed19fadfe588f9b133ad8
Eelis <32396100+ejronty@users.noreply.github.com>
2019-02-18 12:01:24 +0200

    Minor improvement to course administration page

--- src/features/courseAdministrationPage/courseAdministrationPage.tsx
+++ src/features/courseAdministrationPage/courseAdministrationPage.tsx
@@ -5,7 +5,12 @@ import Chapter from '../coursePage/components/Chapter'
 export function courseAdministrationPage() {
   const app = () => {
     return (
-      <p>Hello world</p>
+      <div className="courseAdministrationPageContainer">
+        <h2 className="courseAdministrationPageContainer-heading">Kurssiesi tulostaulut:</h2>
+        <Chapter header="Testikurssi">
+          <p>Dummy-dataa</p>
+        </Chapter>
+      </div>
     )
   }
 
