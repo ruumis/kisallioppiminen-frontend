f2b2890eba35a9916c8cf5aa0dc35b82aff0c48e
Eelis <32396100+ejronty@users.noreply.github.com>
2019-02-18 15:44:44 +0200

    Made static scoreboard table bigger

--- src/features/courseAdministrationPage/components/Scoreboard.tsx
+++ src/features/courseAdministrationPage/components/Scoreboard.tsx
@@ -5,39 +5,113 @@ export default function Scoreboard() {
   return (
     <div className="scoreboard">
       <table>
-        <tr>
-          <th>username</th>
-          <th>1</th>
-          <th>2</th>
-          <th>3</th>
-          <th>4</th>
-          <th>5</th>
-          <th>6</th>
-          <th>7</th>
-          <th>8</th>
-        </tr>
-        <tr>
-          <td>testi Teppo</td>
-          <td><Light color="green" /></td>
-          <td><Light color="gray" /></td>
-          <td><Light color="yellow" /></td>
-          <td><Light color="gray" /></td>
-          <td><Light color="gray" /></td>
-          <td><Light color="red" /></td>
-          <td><Light color="gray" /></td>
-          <td><Light color="gray" /></td>
-        </tr>
-        <tr>
-          <td>koehenkilo</td>
-          <td><Light color="green" /></td>
-          <td><Light color="gray" /></td>
-          <td><Light color="gray" /></td>
-          <td><Light color="green" /></td>
-          <td><Light color="gray" /></td>
-          <td><Light color="gray" /></td>
-          <td><Light color="red" /></td>
-          <td><Light color="yellow" /></td>
-        </tr>
+        <tbody>
+          <tr>
+            <th>username</th>
+            <th>1</th>
+            <th>2</th>
+            <th>3</th>
+            <th>4</th>
+            <th>5</th>
+            <th>6</th>
+            <th>7</th>
+            <th>8</th>
+            <th>9</th>
+            <th>0</th>
+            <th>1</th>
+            <th>2</th>
+            <th>3</th>
+            <th>4</th>
+            <th>5</th>
+            <th>6</th>
+            <th>7</th>
+            <th>8</th>
+            <th>9</th>
+            <th>0</th>
+            <th>1</th>
+            <th>2</th>
+            <th>3</th>
+            <th>4</th>
+            <th>5</th>
+            <th>6</th>
+            <th>7</th>
+            <th>8</th>
+            <th>9</th>
+            <th>0</th>
+            <th>1</th>
+            <th>2</th>
+          </tr>
+          <tr>
+            <td>testi Teppo</td>
+            <td><Light color="green" /></td>
+            <td><Light color="gray" /></td>
+            <td><Light color="yellow" /></td>
+            <td><Light color="gray" /></td>
+            <td><Light color="gray" /></td>
+            <td><Light color="red" /></td>
+            <td><Light color="gray" /></td>
+            <td><Light color="gray" /></td>
+            <td><Light color="green" /></td>
+            <td><Light color="gray" /></td>
+            <td><Light color="yellow" /></td>
+            <td><Light color="gray" /></td>
+            <td><Light color="green" /></td>
+            <td><Light color="red" /></td>
+            <td><Light color="gray" /></td>
+            <td><Light color="gray" /></td>
+            <td><Light color="green" /></td>
+            <td><Light color="gray" /></td>
+            <td><Light color="yellow" /></td>
+            <td><Light color="gray" /></td>
+            <td><Light color="red" /></td>
+            <td><Light color="red" /></td>
+            <td><Light color="gray" /></td>
+            <td><Light color="yellow" /></td>
+            <td><Light color="green" /></td>
+            <td><Light color="gray" /></td>
+            <td><Light color="yellow" /></td>
+            <td><Light color="gray" /></td>
+            <td><Light color="gray" /></td>
+            <td><Light color="red" /></td>
+            <td><Light color="gray" /></td>
+            <td><Light color="red" /></td>
+          </tr>
+          <tr>
+            <td>koehenkilo</td>
+            <td><Light color="green" /></td>
+            <td><Light color="gray" /></td>
+            <td><Light color="gray" /></td>
+            <td><Light color="green" /></td>
+            <td><Light color="gray" /></td>
+            <td><Light color="gray" /></td>
+            <td><Light color="red" /></td>
+            <td><Light color="yellow" /></td>
+            <td><Light color="green" /></td>
+            <td><Light color="gray" /></td>
+            <td><Light color="yellow" /></td>
+            <td><Light color="gray" /></td>
+            <td><Light color="gray" /></td>
+            <td><Light color="red" /></td>
+            <td><Light color="gray" /></td>
+            <td><Light color="gray" /></td>
+            <td><Light color="green" /></td>
+            <td><Light color="gray" /></td>
+            <td><Light color="yellow" /></td>
+            <td><Light color="gray" /></td>
+            <td><Light color="gray" /></td>
+            <td><Light color="red" /></td>
+            <td><Light color="gray" /></td>
+            <td><Light color="gray" /></td>
+            <td><Light color="green" /></td>
+            <td><Light color="gray" /></td>
+            <td><Light color="yellow" /></td>
+            <td><Light color="gray" /></td>
+            <td><Light color="gray" /></td>
+            <td><Light color="red" /></td>
+            <td><Light color="yellow" /></td>
+            <td><Light color="green" /></td>
+          </tr>
+        </tbody>
       </table>
     </div>
   )
