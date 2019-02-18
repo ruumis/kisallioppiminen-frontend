92eb03391cd5cc3007c0b692951b3bccb7fe1d88
Eelis <32396100+ejronty@users.noreply.github.com>
2019-02-18 15:03:45 +0200

    First draft of scoreboard trafficlights

--- src/features/courseAdministrationPage/courseAdministrationPage.tsx
+++ src/features/courseAdministrationPage/courseAdministrationPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import Chapter from '../coursePage/components/Chapter'
+import Scoreboard from './components/Scoreboard'
 
 export function courseAdministrationPage() {
   const app = () => {
@@ -8,7 +9,7 @@ export function courseAdministrationPage() {
       <div className="courseAdministrationPageContainer">
         <h2 className="courseAdministrationPageContainer-heading">Kurssiesi tulostaulut:</h2>
         <Chapter header="Testikurssi">
-          <p>Dummy-dataa</p>
+          <Scoreboard />
         </Chapter>
       </div>
     )
