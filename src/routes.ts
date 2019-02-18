c1fd7c201bede18869406186f42610d962e17d2f
Eelis <32396100+ejronty@users.noreply.github.com>
2019-02-18 10:46:12 +0200

    Created course administration page (no content yet)

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { frontPage } from './features/frontpage/frontPage'
 import { coursePage } from './features/coursePage/coursePage'
 import { infoPage } from './features/infoPage/infoPage'
+import { courseAdministrationPage } from './features/courseAdministrationPage/courseAdministrationPage'
 import { InitialState } from './types/InitialState'
 import { Store, AnyAction } from 'redux'
 import { changePage } from './reducers/actions/pageStateActions'
@@ -21,8 +22,15 @@ const routes: Array<{ path: Path; component: () => JSX.Element; pageName: string
     path: new Path('/tietoa'),
     component: infoPage,
     pageName: 'Tietoa'
+  },
+  {
+    path: new Path('/courseAdmin'),
+    component: courseAdministrationPage,
+    pageName: 'Kurssihallinta'
   }
 ]
+
+// courseAdmin sivu yllä ei ole lopullinen reitti!!
 
 export function getPage(currentPath: string): { component: () => JSX.Element; pathParams: any; pageName: string } | undefined {
   const selectedRoute = routes.find(route => route.path.test(currentPath) !== null)
