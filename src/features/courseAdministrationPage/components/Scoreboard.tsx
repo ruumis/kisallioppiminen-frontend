e4e45e1db796d634a639695de4531f276ae5c4e0
Eelis <32396100+ejronty@users.noreply.github.com>
2019-02-19 09:49:53 +0200

    Improved style of scoreboards

--- src/features/courseAdministrationPage/components/Scoreboard.tsx
+++ src/features/courseAdministrationPage/components/Scoreboard.tsx
@@ -7,39 +7,39 @@ export default function Scoreboard() {
       <table>
         <tbody>
           <tr>
-            <th>username</th>
-            <th>1</th>
-            <th>2</th>
-            <th>3</th>
-            <th>4</th>
-            <th>5</th>
-            <th>6</th>
-            <th>7</th>
-            <th>8</th>
-            <th>9</th>
-            <th>0</th>
-            <th>1</th>
-            <th>2</th>
-            <th>3</th>
-            <th>4</th>
-            <th>5</th>
-            <th>6</th>
-            <th>7</th>
-            <th>8</th>
-            <th>9</th>
-            <th>0</th>
-            <th>1</th>
-            <th>2</th>
-            <th>3</th>
-            <th>4</th>
-            <th>5</th>
-            <th>6</th>
-            <th>7</th>
-            <th>8</th>
-            <th>9</th>
-            <th>0</th>
-            <th>1</th>
-            <th>2</th>
+            <th> </th>
+            <th>1.1</th>
+            <th>1.2</th>
+            <th>1.3</th>
+            <th>1.4</th>
+            <th>1.5</th>
+            <th>1.6</th>
+            <th>1.7</th>
+            <th>1.8</th>
+            <th>1.9</th>
+            <th>2.0</th>
+            <th>2.1</th>
+            <th>2.2</th>
+            <th>2.3</th>
+            <th>2.4</th>
+            <th>2.5</th>
+            <th>2.6</th>
+            <th>2.7</th>
+            <th>2.8</th>
+            <th>2.9</th>
+            <th>3.0</th>
+            <th>3.1</th>
+            <th>3.2</th>
+            <th>3.3</th>
+            <th>3.4</th>
+            <th>3.5</th>
+            <th>3.6</th>
+            <th>3.7</th>
+            <th>3.8</th>
+            <th>3.9</th>
+            <th>4.0</th>
+            <th>4.1</th>
+            <th>4.2</th>
           </tr>
           <tr>
             <td>testi Teppo</td>
