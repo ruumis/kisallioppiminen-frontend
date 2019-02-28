d99c46efce37348ac62cb65da0c785b0a8665440
Etsku520 <eetu.huovila@gmail.com>
2019-02-28 12:57:07 +0200

    make script work no matter where it's called from (it still breaks if you move the script file)

--- scripts/createUids.js
+++ scripts/createUids.js
@@ -1,7 +1,14 @@
 const fs = require('fs')
 const uuidv1 = require('uuid/v1')
+const path = require('path')
 
 const createUids = path => {
+  const end = path.substring(path.length - 3)
+
+  if (end !== 'idl') {
+    return false
+  }
+
   const text = fs.readFileSync(path).toString()
   const textByLine = text.split('\n')
   const uidTextByLine = textByLine.map(line => {
@@ -20,4 +27,17 @@ const createUids = path => {
   })
 }
 
-createUids(process.argv[2])
+goThroughFiles = base => {
+  fs.readdirSync(base).forEach(file => {
+    file = path.resolve(base, file)
+    fs.stat(file, (err, stat) => {
+      if (stat && stat.isDirectory()) {
+        goThroughFiles(file)
+      } else {
+        createUids(file)
+      }
+    })
+  })
+}
+
+goThroughFiles(`${__dirname}/../content`)
