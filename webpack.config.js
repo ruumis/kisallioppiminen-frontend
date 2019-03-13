b1886302302d37d5a205ce8caee23c1798babd5e
Etsku520 <eetu.huovila@gmail.com>
2019-03-13 18:51:43 +0200

    images to static

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+const fs = require('fs')
 
 module.exports = function(env, argv) {
-  
   // default to the server configuration
   const base = {
     entry: './src/server/server.ts',
@@ -11,13 +11,13 @@ module.exports = function(env, argv) {
       filename: 'js/server.js',
       // path needs to be an ABSOLUTE file path
       path: path.resolve(process.cwd(), 'dist'),
-      publicPath: '/',
+      publicPath: '/'
     },
     // Enable sourcemaps for debugging webpack's output.
     devtool: 'cheap-module-eval-source-map',
     resolve: {
       // Add '.ts' and '.tsx' as resolvable extensions.
-      extensions: [".ts", ".tsx", ".js", ".json"],
+      extensions: ['.ts', '.tsx', '.js', '.json']
     },
     module: {
       rules: [
@@ -26,7 +26,7 @@ module.exports = function(env, argv) {
           test: /\.tsx?$/,
           use: [
             {
-              loader: 'ts-loader',
+              loader: 'ts-loader'
             }
           ]
         }
@@ -35,54 +35,80 @@ module.exports = function(env, argv) {
     plugins: [
       new ExtractTextPlugin({
         filename: 'css/style.css',
-        allChunks: true,
+        allChunks: true
       }),
-  		new CopyWebpackPlugin([
-  		  // relative path is from src
-				{ from: 'favicon.ico' }, // <- your path to favicon
-  		])
+      new CopyWebpackPlugin([
+        // relative path is from src
+        { from: 'favicon.ico' } // <- your path to favicon
+      ])
     ]
   }
 
   if (env.platform === 'styles') {
-    base.module.rules = [{
-      test: /\.(scss|sass)$/,
-      use: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
-    }]
-    base.entry = './styles/style.scss';
-    base.output.filename = 'css/style.css';
+    base.module.rules = [
+      {
+        test: /\.(scss|sass)$/,
+        use: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+      }
+    ]
+    base.entry = './styles/style.scss'
+    base.output.filename = 'css/style.css'
   }
 
   if (env.platform === 'styles-static') {
-    base.module.rules = [{
-      test: /\.(scss|sass)$/,
-      use: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
-    }]
+    base.module.rules = [
+      {
+        test: /\.(scss|sass)$/,
+        use: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+      }
+    ]
     base.plugins = [
       new ExtractTextPlugin({
         filename: 'static/css/style.css',
-        allChunks: true,
-      }),
+        allChunks: true
+      })
     ]
-    base.entry = './styles/style.scss';
-    base.output.filename = 'static/css/style.css';
+    base.entry = './styles/style.scss'
+    base.output.filename = 'static/css/style.css'
   }
-  
+
   // server-specific configuration
   if (env.platform === 'server') {
-    base.target = 'node';
+    base.target = 'node'
   }
 
   // client-specific configurations
   if (env.platform === 'web') {
-    base.entry = './src/client.tsx';
-    base.output.filename = 'js/client.js';
+    base.entry = './src/client.tsx'
+    base.output.filename = 'js/client.js'
   }
 
   if (env.platform === 'static') {
-    base.entry = './src/client.tsx';
+    base.entry = './src/client.tsx'
     base.output.filename = 'static/client.js'
   }
 
-  return base;
+  if (env.platform === 'static') {
+    const goThroughFiles = base => {
+      fs.readdirSync(base).forEach(file => {
+        const newPath = path.resolve(base, file)
+        fs.stat(newPath, (err, stat) => {
+          if (stat && stat.isDirectory()) {
+            goThroughFiles(newPath)
+          } else {
+            moveImage(newPath, file)
+          }
+        })
+      })
+    }
+
+    const moveImage = (newPath, file) => {
+      const image = fs.readFileSync(newPath)
+      fs.writeFileSync(`./dist/static/img/${file}`, image)
+    }
+
+    goThroughFiles(`${__dirname}/dist/img`)
+  }
+
+  return base
 }
